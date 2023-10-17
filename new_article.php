d1d4bb8693fe7755e4d45fe5ee065ac4d0c50d68
Marjan <97312708+Marjanamiiri@users.noreply.github.com>
2023-10-16 21:01:30 -0700

    add error masseges and modify code

--- new_article.php
+++ new_article.php
@@ -4,21 +4,35 @@ require_once 'src/ArticleRepository.php';
 require_once 'src/Models/Article.php';
 require_once 'helpers/helpers.php';
 
+$errors = []; 
+
 if (isset($_POST['submit'])) {
     $title = $_POST['title'];
     $url = $_POST['url'];
-    // Create a new Article instance
-    $newArticle = new Article(time(), $title, $url);
-    // Create an instance of ArticleRepository
-    $articleRepository = new ArticleRepository('articles.json');
-    // Save the new article
-    $articleRepository->saveArticle($newArticle);
-    header('Location: index.php');
-    exit();
+
+    if (!preg_match('/^https?:\/\/[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}/', $url)) {
+        $errors['url'] = 'Invalid URL format. Please enter a valid URL.';
+    }
+    if (empty($url)) {
+        $errors['url'] = 'url is required.';
+    }
+    if (empty($title)) {
+        $errors['title'] = 'Title is required.';
+    }
+
+    if (empty($errors)) {
+        // Create a new Article instance
+        $newArticle = new Article(time(), $title, $url);
+        // Create an instance of ArticleRepository
+        $articleRepository = new ArticleRepository('articles.json');
+        // Save the new article
+        $articleRepository->saveArticle($newArticle);
+        header('Location: index.php');
+        exit();
+    }
 }
 
 ?>
-
 <!DOCTYPE html>
 <html lang="en">
 <?php require_once 'layout/header.php' ?>
@@ -32,9 +46,15 @@ if (isset($_POST['submit'])) {
                 <form method="POST">
                     <div class="form-group">
                         <input id="add-title" name="title" type="text" class="form-control" placeholder="Add Title" required>
+                        <?php if (isset($errors['title'])) : ?>
+                            <div class="error-message"><?php echo $errors['title']; ?></div>
+                        <?php endif; ?>
                     </div>
                     <div class="form-group">
                         <input id="add-url" name="url" type="text" class="form-control" placeholder="Add URL" required>
+                        <?php if (isset($errors['url'])) : ?>
+                            <div class="error-message"><?php echo $errors['url']; ?></div>
+                        <?php endif; ?>
                     </div>
                     <button type="submit" name="submit" class="btn btn-secondary btn-block">Submit</button>
                 </form>
@@ -42,5 +62,4 @@ if (isset($_POST['submit'])) {
         </div>
     </div>
 </body>
-
 </html>
--- update_article.php
+++ update_article.php
@@ -1,8 +1,10 @@
 <?php
-
 require_once 'src/ArticleRepository.php';
 require_once 'src/Models/Article.php';
 require_once 'helpers/helpers.php';
+
+$errors = [];
+$validUrl = true; // Assume the URL is initially valid
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['id'])) {
     $articleId = (int)$_POST['id'];
@@ -13,14 +15,23 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['id'])) {
     $article = $articleRepository->getArticleById($articleId);
 
     if ($article) {
-        // Update the article with the new title and URL
-        $article->setTitle($newTitle);
-        $article->setUrl($newUrl);
-        $articleRepository->updateArticle($articleId, $article);
+        // Validate the new URL format
+        if (!preg_match('/^https?:\/\/[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}/', $newUrl)) {
+            $errors['url'] = 'Invalid URL format. Please enter a valid URL.';
+            $validUrl = false;
+        }
 
-        // Redirect back to the article list
-        header('Location: index.php');
-        exit();
+        // Check if the URL is valid before updating the article
+        if ($validUrl) {
+            // Update the article with the new title and URL
+            $article->setTitle($newTitle);
+            $article->setUrl($newUrl);
+            $articleRepository->updateArticle($articleId, $article);
+
+            // Redirect back to the article list
+            header('Location: index.php');
+            exit();
+        }
     }
 } elseif (isset($_GET['id'])) {
     $articleId = $_GET['id'];
@@ -32,30 +43,34 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['id'])) {
         $title = $article->getTitle();
         $url = $article->getUrl();
     }
-
 }
-
-// Display the edit form
 ?>
-<!doctype html>
+
+<!DOCTYPE html>
 <html lang="en">
 <?php require_once 'layout/header.php' ?>
 
 <body>
     <?php require_once 'layout/navigation.php' ?>
-
     <div class="container mt-5">
-        <h2 class="text-center font-semibold text-indigo-700 mt-10">Edit Article</h2>
-        <form method="POST">
-            <div class="form-group">
-                <input id="edit-title" name="title" type="text" class="form-control" placeholder="Edit title" value="<?= $title; ?>" required>
+        <div class="row justify-content-center">
+            <div class="col-md-6">
+                <h2 class="text-center mb-4">Edit Article</h2>
+                <?php if (isset($errors['url'])) : ?>
+                    <div class="error-message"><?php echo $errors['url']; ?></div>
+                <?php endif; ?>
+                <form method="POST">
+                    <input type="hidden" name="id" value="<?php echo $articleId; ?>">
+                    <div class="form-group">
+                        <input id="edit-title" name="title" type="text" class="form-control" placeholder="Edit Title" value="<?php echo isset($title) ? $title : ''; ?>" required>
+                    </div>
+                    <div class="form-group">
+                        <input id="edit-url" name="url" type="text" class="form-control" placeholder="Edit URL" value="<?php echo isset($url) ? $url : ''; ?>" required>
+                    </div>
+                    <button type="submit" name="submit" class="btn btn-secondary btn-block">Save</button>
+                </form>
             </div>
-            <div class="form-group">
-                <input id="edit-url" name="url" type="text" class="form-control" placeholder="Edit URL" value="<?= $url; ?>" required>
-            </div>
-            <button type="submit" class="btn btn-primary">Update</button>
-            <input type="hidden" name="id" value="<?= $articleId; ?>">
-        </form>
+        </div>
     </div>
 </body>
 </html>
